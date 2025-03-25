91169628da0e823cf514fe76b5e08d736104d7ec
Mikhail Nyuskhaev <kuytest@gmail.com>
2025-03-25 12:09:10 +0300

    new wrapper for port-method

--- internal/app/commands/port.go
+++ internal/app/commands/port.go
@@ -1,32 +1,17 @@
 package commands
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"os/signal"
-	"syscall"
 	"telebot/internal/service/wrapper"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	pb "github.com/tinkoff/invest-api-go-sdk/proto"
-
-	"github.com/tinkoff/invest-api-go-sdk/investgo"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 func (c *Commander) GetPort(inputMessage *tgbotapi.Message) {
-	config, err := investgo.LoadConfig("cmd/bot/config.yaml")
-	if err != nil {
-		log.Fatalf("config loading error %v", err.Error())
-	}
-
-	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
-	defer cancel()
-	// сдк использует для внутреннего логирования investgo.Logger
-	// для примера передадим uber.zap
 	zapConfig := zap.NewDevelopmentConfig()
 	zapConfig.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout(time.DateTime)
 	zapConfig.EncoderConfig.TimeKey = "time"
@@ -41,52 +26,77 @@ func (c *Commander) GetPort(inputMessage *tgbotapi.Message) {
 	if err != nil {
 		log.Fatalf("logger creating error %v", err)
 	}
-	// создаем клиента для investAPI, он позволяет создавать нужные сервисы и уже
-	// через них вызывать нужные методы
-	client, err := investgo.NewClient(ctx, config, logger)
+
+	// активируем клиента InvestAPI
+	client, config, cancel, err := wrapper.ActivateInvest(logger)
 	if err != nil {
 		logger.Fatalf("client creating error %v", err.Error())
 	}
 	// убрали закрытие клиента в отдельный метод
-	defer func() {
-		logger.Infof("closing client connection")
-		err := client.Stop()
-		if err != nil {
-			logger.Errorf("client shutdown error %v", err.Error())
-		}
-	}()
+	// defer func() {
+	// 	main.Logger.Infof("closing client connection")
+	// 	err := client.Stop()
+	// 	if err != nil {
+	// 		main.Logger.Errorf("client shutdown error %v", err.Error())
+	// 	}
+	// }()
+
+	// deferim клиента InvestAPI и отменяем контекст
+	defer wrapper.SwitchoffInvest(logger, client, cancel)
 
 	// создаем клиента операций для сбора данных по портфелю
-	operationsService := client.NewOperationsServiceClient()
+	operationsService := wrapper.CreateOperationsClient(client)
+
+	// создаем клиента операций для сбора данных по портфелю
+	// operationsService := client.NewOperationsServiceClient()
+
+	// создаем пустую позицию
+	positions := wrapper.NewPosition()
+
+	// загружаем позицию
+	positions.Upload(client, operationsService, config, logger)
+
+	// выводим сообщение позиции в ТГ
+	text := fmt.Sprintf("Стоимость портфеля: %.2f\n", positions.Amount)
+	for i := range positions.Bonds {
+		text += fmt.Sprintf("Позиция %d: %s текущая стоимость: %.2f\n",
+			i,
+			positions.Bonds[i].Title,
+			positions.Bonds[i].CurSumm)
+	}
+
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, text)
+	c.bot.Send(msg)
 
 	// запрашиваем портфель в рублях и отдаем стоимость активов (облиги например)
-	portfolioResp, err := operationsService.GetPortfolio(config.AccountId, pb.PortfolioRequest_RUB)
-	if err != nil {
-		logger.Errorf(err.Error())
-	} else {
-		// юниты - целая часть, нано - дробная. В сообщение собираем стоимость портфеля просто через точку
-		units := portfolioResp.TotalAmountPortfolio.Units
-		nano := portfolioResp.TotalAmountPortfolio.Nano
-		dohunits := portfolioResp.ExpectedYield.Units
-		dohnano := portfolioResp.ExpectedYield.Nano
-		var positions string
-		for i := range portfolioResp.Positions {
-			positions += fmt.Sprintf("Позиция %d: %s %d"+"."+"%d\n",
-				i,
-				wrapper.TakeNameUID(portfolioResp.Positions[i].InstrumentUid, client, logger),
-				portfolioResp.Positions[i].Quantity.Units,
-				portfolioResp.Positions[i].Quantity.Nano/10000000)
-		}
+	// portfolioResp, err := operationsService.GetPortfolio(config.AccountId, pb.PortfolioRequest_RUB)
+	// if err != nil {
+	// 	logger.Errorf(err.Error())
+	// } else {
+	// 	// юниты - целая часть, нано - дробная. В сообщение собираем стоимость портфеля просто через точку
+	// 	units := portfolioResp.TotalAmountPortfolio.Units
+	// 	nano := portfolioResp.TotalAmountPortfolio.Nano
+	// 	dohunits := portfolioResp.ExpectedYield.Units
+	// 	dohnano := portfolioResp.ExpectedYield.Nano
+	// 	var positions string
+	// 	for i := range portfolioResp.Positions {
+	// 		positions += fmt.Sprintf("Позиция %d: %s %d"+"."+"%d\n",
+	// 			i,
+	// 			wrapper.TakeNameUID(portfolioResp.Positions[i].InstrumentUid, client, logger),
+	// 			portfolioResp.Positions[i].Quantity.Units,
+	// 			portfolioResp.Positions[i].Quantity.Nano/10000000)
+	// 	}
 
-		text := fmt.Sprintf("Стоимость портфеля %s:\n%d"+"."+"%d\n"+"%d"+"."+"%d",
-			config.AccountId,
-			units,
-			nano/10000000,
-			dohunits,
-			dohnano/10000000)
-		msg_2 := tgbotapi.NewMessage(inputMessage.Chat.ID, text)
-		msg_3 := tgbotapi.NewMessage(inputMessage.Chat.ID, positions)
-		c.bot.Send(msg_2)
-		c.bot.Send(msg_3)
-	}
+	// 	text := fmt.Sprintf("Стоимость портфеля %s:\n%d"+"."+"%d\n"+"%d"+"."+"%d",
+	// 		config.AccountId,
+	// 		units,
+	// 		nano/10000000,
+	// 		dohunits,
+	// 		dohnano/10000000)
+	// 	msg_2 := tgbotapi.NewMessage(inputMessage.Chat.ID, text)
+	// 	msg_3 := tgbotapi.NewMessage(inputMessage.Chat.ID, positions)
+	// 	c.bot.Send(msg_2)
+	// 	c.bot.Send(msg_3)
+	// }
+
 }
--- internal/service/wrapper/wrapper.go
+++ internal/service/wrapper/wrapper.go
@@ -1,15 +1,104 @@
 package wrapper
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"os/signal"
+	"strconv"
+	"syscall"
+
+	pb "github.com/tinkoff/invest-api-go-sdk/proto"
+
 	"github.com/tinkoff/invest-api-go-sdk/investgo"
 	"go.uber.org/zap"
 )
 
-// type Position []struct {
-// 	Title    string
-// 	Count    int
-// 	CurPrice int
-// }
+type Position struct {
+	Amount float64
+	Bonds  []Bonds
+}
+
+type Bonds struct {
+	ExternalId int
+	UID        string
+	Title      string
+	Count      float64
+	CurPrice   float64
+	CurSumm    float64
+}
+
+// создает структуру позиций
+func NewPosition() *Position {
+	return &Position{}
+}
+
+// создаем клиента для investAPI, он позволяет создавать нужные сервисы
+func ActivateInvest(logger *zap.SugaredLogger) (*investgo.Client, *investgo.Config, context.CancelFunc, error) {
+	config, err := investgo.LoadConfig("cmd/bot/config.yaml")
+	if err != nil {
+		log.Fatalf("config loading error %v", err.Error())
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+
+	client, err := investgo.NewClient(ctx, config, logger)
+	if err != nil {
+		logger.Fatalf("client creating error %v", err.Error())
+	}
+
+	return client, &config, cancel, err
+}
+
+// закрывает клиенты для InvestAPI командой
+func SwitchoffInvest(logger *zap.SugaredLogger, client *investgo.Client, cancel context.CancelFunc) {
+	logger.Infof("closing client connection")
+	err := client.Stop()
+	if err != nil {
+		logger.Errorf("client shutdown error %v", err.Error())
+	}
+
+	defer cancel()
+}
+
+// создаем клиента для работы с портфелем
+func CreateOperationsClient(client *investgo.Client) *investgo.OperationsServiceClient {
+	operationsService := client.NewOperationsServiceClient()
+	return operationsService
+}
+
+// базово заливает позиции по счету
+func (p *Position) Upload(client *investgo.Client, opClient *investgo.OperationsServiceClient, config *investgo.Config, logger *zap.SugaredLogger) {
+	portfolioResp, err := opClient.GetPortfolio(config.AccountId, pb.PortfolioRequest_RUB)
+	if err != nil {
+		logger.Errorf(err.Error())
+	} else {
+		amountP := MoneyFloat64(portfolioResp.TotalAmountPortfolio.Units, portfolioResp.TotalAmountPortfolio.Nano)
+		p.Amount = amountP
+		for i := range portfolioResp.Positions {
+			p.Bonds = append(p.Bonds, Bonds{
+				ExternalId: i,
+				UID:        portfolioResp.Positions[i].InstrumentUid,
+				Title:      TakeNameUID(portfolioResp.Positions[i].InstrumentUid, client, logger),
+				Count:      MoneyFloat64(portfolioResp.Positions[i].Quantity.Units, portfolioResp.Positions[i].Quantity.Nano),
+				CurPrice:   MoneyFloat64(portfolioResp.Positions[i].CurrentPrice.Units, portfolioResp.Positions[i].CurrentPrice.Nano),
+				CurSumm: MoneyFloat64(portfolioResp.Positions[i].Quantity.Units, portfolioResp.Positions[i].Quantity.Nano) *
+					MoneyFloat64(portfolioResp.Positions[i].CurrentPrice.Units, portfolioResp.Positions[i].CurrentPrice.Nano),
+			})
+		}
+	}
+}
+
+// чистит позиции структуры
+func (p *Position) Clear() {
+	p = &Position{}
+}
+
+// чистит позицию и снова загружает (использует оба доступных метода)
+func (p *Position) Update(client *investgo.Client, opClient *investgo.OperationsServiceClient, config *investgo.Config, logger *zap.SugaredLogger) {
+	p.Clear()
+	p.Upload(client, opClient, config, logger)
+}
 
 func TakeNameUID(uid string, client *investgo.Client, logger *zap.SugaredLogger) string {
 	instrumentsService := client.NewInstrumentsServiceClient()
@@ -21,6 +110,11 @@ func TakeNameUID(uid string, client *investgo.Client, logger *zap.SugaredLogger)
 	return name.Instrument.Name
 }
 
-func MoneyString(units, nano int) {
-
+func MoneyFloat64(units int64, nano int32) float64 {
+	unitsF := float64(units)
+	nanoF, err := strconv.ParseFloat(fmt.Sprintf("0.%d", nano/10_000_000), 32)
+	if err != nil {
+		return 0.0
+	}
+	return unitsF + nanoF
 }
